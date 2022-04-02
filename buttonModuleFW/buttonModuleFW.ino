0d3f03d62a6ddfe1b8da4c03daea8b6352a099de
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2022-04-02 16:21:12 -0400

    change buttonState to hex

--- buttonModuleFW/buttonModuleFW.ino
+++ buttonModuleFW/buttonModuleFW.ino
@@ -23,11 +23,11 @@ void setup() {
 
 void loop() {
   int noButton = 0;
-  if (digitalRead(21)) buttonState = 1; else noButton += 1;
-  if (digitalRead(22)) buttonState = 2; else noButton += 1;
-  if (digitalRead(23)) buttonState = 3; else noButton += 1;
-  if (digitalRead(24)) buttonState = 4; else noButton += 1;
-  if (noButton >= 4) buttonState = 0;
+  if (digitalRead(21)) buttonState = 0x1; else noButton += 1;
+  if (digitalRead(22)) buttonState = 0x2; else noButton += 1;
+  if (digitalRead(23)) buttonState = 0x3; else noButton += 1;
+  if (digitalRead(24)) buttonState = 0x4; else noButton += 1;
+  if (noButton >= 4) buttonState = 0x0;
   canMsg1.can_id  = 0x210;
   canMsg1.can_dlc = 1;
   canMsg1.data[0] = heartBeat;
